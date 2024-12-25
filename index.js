5cb07129a20ec659780898520fb2421a8db7b026
muhammadbilal127 <mbilal52257@gmail.com>
2024-12-26 02:12:44 +0500

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+
+ var yourname = prompt ("enter your name")
+ alert ("welcome my website" + yourname )
+ 
+ var  numberone = prompt("enter your numberone")
+ var numbertwo = prompt("enter your numbertwo")
+ 
+ 
+ var result = numberone + numbertwo;
+ 
+ alert(numberone + numbertwo)
+ 
